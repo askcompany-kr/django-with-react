a44c8df55a323b26775d33bca3006409b6f4eea2
Chinseok Lee <me@askcompany.kr>
2020-02-01 23:51:53 +0900

    instagram 앱 생성 및 Post 모델 마이그레이션

--- askcompany/urls.py
+++ askcompany/urls.py
@@ -19,4 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('blog1/', include('blog1.urls')),
+    path('instagram/', include('instagram.urls')),
 ]
---
+++ instagram/models.py
@@ -0,0 +1,7 @@
+from django.db import models
+
+
+class Post(models.Model):
+    message = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
