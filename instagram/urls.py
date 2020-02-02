0e5378dbe9358a573a7a1c58f0eef9610b6d805a
Chinseok Lee <me@askcompany.kr>
2020-02-02 14:56:31 +0900

    Queryset를 활용한 간단검색 구현

--- instagram/urls.py
+++ instagram/urls.py
@@ -1,3 +1,7 @@
+from django.urls import path
+
+from . import views
 
 urlpatterns = [
+    path('', views.post_list),
 ]
--- instagram/views.py
+++ instagram/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from .models import Post
 
-# Create your views here.
+
+def post_list(request):
+    qs = Post.objects.all()
+    q = request.GET.get('q', '')
+    if q:
+        qs = qs.filter(message__icontains=q)
+    # instagram/templates/instagram/post_list.html
+    return render(request, 'instagram/post_list.html', {
+        'post_list': qs,
+        'q': q,
+    })
