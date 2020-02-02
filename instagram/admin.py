0153d7bf0aa0c32d2163dc5fe4da38cd6a417d03
Chinseok Lee <me@askcompany.kr>
2020-02-02 09:49:21 +0900

    instagram Post 모델을 admin에 등록하고, is_public 필드 추가

--- instagram/admin.py
+++ instagram/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Post
 
-# Register your models here.
+
+@admin.register(Post)  # Wrapping
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['id', 'message', 'message_length', 'is_public', 'created_at', 'updated_at']
+    list_display_links = ['message']
+    list_filter = ['created_at', 'is_public']
+    search_fields = ['message']
+
+    def message_length(self, post):
+        return f"{len(post.message)} 글자"
--- instagram/models.py
+++ instagram/models.py
@@ -3,5 +3,16 @@ from django.db import models
 
 class Post(models.Model):
     message = models.TextField()
+    is_public = models.BooleanField(default=False, verbose_name='공개여부')
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    # Java의 toString
+    def __str__(self):
+        # return f"Custom Post object ({self.id})"
+        # return "Custom Post object ({})".format(self.id)
+        return self.message
+
+    # def message_length(self):
+    #     return len(self.message)
+    # message_length.short_description = "메세지 글자수"
