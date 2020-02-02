06629baddc2f6faafe0ae292e4381da1911d0ef3
Chinseok Lee <me@askcompany.kr>
2020-02-02 17:09:09 +0900

    ForeignKey 예시 추가

--- instagram/admin.py
+++ instagram/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.utils.safestring import mark_safe
 
-from .models import Post
+from .models import Post, Comment
 
 
 @admin.register(Post)  # Wrapping
@@ -18,3 +18,8 @@ class PostAdmin(admin.ModelAdmin):
 
     def message_length(self, post):
         return f"{len(post.message)} 글자"
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    pass
--- instagram/models.py
+++ instagram/models.py
@@ -1,7 +1,9 @@
+from django.conf import settings
 from django.db import models
 
 
 class Post(models.Model):
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     message = models.TextField()
     photo = models.ImageField(blank=True, upload_to='instagram/post/%Y/%m/%d')
     is_public = models.BooleanField(default=False, verbose_name='공개여부')
@@ -20,3 +22,11 @@ class Post(models.Model):
     # def message_length(self):
     #     return len(self.message)
     # message_length.short_description = "메세지 글자수"
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE,
+                             limit_choices_to={'is_public': True})  # post_id 필드가 생성이 됩니다.
+    message = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
