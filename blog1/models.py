afa65690e52e7108b81029528d70a0e7a2a6c28e
Chinseok Lee <me@askcompany.kr>
2020-02-02 19:27:56 +0900

    makemigrations 시 필수필드에 대한 처리

--- blog1/models.py
+++ blog1/models.py
@@ -1,7 +1,10 @@
+from django.conf import settings
 from django.db import models
 
 
 class Post(models.Model):
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE,
+                               related_name='+')
     title = models.CharField(max_length=100)
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
