2215164e6633a78a20c514c4f527e08d38618ba9
Chinseok Lee <me@askcompany.kr>
2020-02-02 15:48:45 +0900

    instagram Post 모델에 기본 정렬을 추가하고, django-extensions 앱을 등록

--- askcompany/settings.py
+++ askcompany/settings.py
@@ -31,12 +31,16 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    # django apps
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # third apps
+    'django_extensions',
+    # locals apps
     'blog1',
     'instagram',
 ]
--- instagram/models.py
+++ instagram/models.py
@@ -14,6 +14,9 @@ class Post(models.Model):
         # return "Custom Post object ({})".format(self.id)
         return self.message
 
+    class Meta:
+        ordering = ['-id']
+
     # def message_length(self):
     #     return len(self.message)
     # message_length.short_description = "메세지 글자수"
