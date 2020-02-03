efb3ec0a4c43f34c11c3db20e681e0c0dc92291b
Chinseok Lee <me@askcompany.kr>
2020-02-03 16:05:54 +0900

    다양한 응답의 함수 기반 뷰

--- instagram/urls.py
+++ instagram/urls.py
@@ -1,7 +1,9 @@
-from django.urls import path
+from django.urls import path, re_path
 
 from . import views
 
 urlpatterns = [
     path('', views.post_list),
+    path('<int:pk>/', views.post_detail),
+    # re_path(r'(?P<pk>\d+)/$', views.post_detail),
 ]
--- instagram/views.py
+++ instagram/views.py
@@ -1,14 +1,27 @@
+from django.views.generic import ListView
+from django.http import HttpResponse, HttpRequest
 from django.shortcuts import render
 from .models import Post
 
 
-def post_list(request):
-    qs = Post.objects.all()
-    q = request.GET.get('q', '')
-    if q:
-        qs = qs.filter(message__icontains=q)
-    # instagram/templates/instagram/post_list.html
-    return render(request, 'instagram/post_list.html', {
-        'post_list': qs,
-        'q': q,
-    })
+post_list = ListView.as_view(model=Post)
+
+
+# def post_list(request):
+#     qs = Post.objects.all()
+#     q = request.GET.get('q', '')
+#     if q:
+#         qs = qs.filter(message__icontains=q)
+#     # instagram/templates/instagram/post_list.html
+#     return render(request, 'instagram/post_list.html', {
+#         'post_list': qs,
+#         'q': q,
+#     })
+
+
+def post_detail(request: HttpRequest, pk: int) -> HttpResponse:
+    response = HttpResponse()
+    response.write("Hello World")
+    response.write("Hello World")
+    response.write("Hello World")
+    return response
