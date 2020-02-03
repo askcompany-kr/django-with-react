57b188564231814d56ed1b9d3d1d34fc7cf439c8
Chinseok Lee <me@askcompany.kr>
2020-02-04 01:46:48 +0900

    bootstrap/bootswatch cdn 버전 적용

--- instagram/views.py
+++ instagram/views.py
@@ -14,7 +14,7 @@ from .models import Post
 # @method_decorator(login_required, name='dispatch')
 class PostListView(LoginRequiredMixin, ListView):
     model = Post
-    paginate_by = 10
+    paginate_by = 100
 
 
 post_list = PostListView.as_view()
