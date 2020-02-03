9d7cc1f877f3d97efd6ffbc90711ec021ff41fec
Chinseok Lee <me@askcompany.kr>
2020-02-04 03:02:39 +0900

    bootstrap/jquery 파일들을 장고 static 경로에 추가

--- askcompany/settings.py
+++ askcompany/settings.py
@@ -134,7 +134,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-# STATIC_ROOT = ''  # TODO
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'askcompany', 'static'),
+]
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
