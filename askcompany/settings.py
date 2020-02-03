83fed86a205147757f4537238b2bf9e1339375c7
Chinseok Lee <me@askcompany.kr>
2020-02-03 22:01:19 +0900

    django-bootstrap4를 통한 페이징 처리 구현

--- askcompany/settings.py
+++ askcompany/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     # third apps
+    'bootstrap4',
     'django_extensions',
     'debug_toolbar',
     # locals apps
