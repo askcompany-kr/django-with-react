0ee7fa560202a33277e8fdf65f6539ad38de9217
Chinseok Lee <me@askcompany.kr>
2020-02-04 15:48:03 +0900

    전역 레이아웃 생성 및 로그인 구현

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,3 +1,6 @@
+from django.contrib.auth.views import LoginView
+from django.urls import path
 
 urlpatterns = [
+    path('login/', LoginView.as_view(template_name='accounts/login_form.html'), name='login'),
 ]
