63bc6c96dde70fb8296759ad8414a8dfa38de4da
Chinseok Lee <me@askcompany.kr>
2020-02-04 16:28:27 +0900

    auth앱의 UserCreationForm을 활용한 회원가입 구현

--- accounts/views.py
+++ accounts/views.py
@@ -1,14 +1,19 @@
+from django.conf import settings
+from django.contrib.auth import get_user_model
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render, redirect
-from django.views.generic import TemplateView
-
+from django.views.generic import TemplateView, CreateView
 
 # @login_required
 # def profile(request):
 #     return render(request, 'accounts/profile.html')
 from accounts.forms import ProfileForm
 from accounts.models import Profile
+
+
+User = get_user_model()
 
 
 class ProfileView(LoginRequiredMixin, TemplateView):
@@ -39,8 +44,12 @@ def profile_edit(request):
     })
 
 
-def signup(request):
-    pass
+signup = CreateView.as_view(
+    model=User,
+    form_class=UserCreationForm,
+    success_url=settings.LOGIN_URL,
+    template_name='accounts/signup_form.html',
+)
 
 
 def logout(request):
